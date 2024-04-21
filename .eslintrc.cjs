9ac5166681e7bf1c2fba76fe96f3c1a7c5504af0
Valera <valerakaktotak@gmail.com>
2024-04-21 18:15:18 +0300

    added ESLint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,74 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: [
+    'react-refresh',
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'simple-import-sort'
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    'consistent-return': 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'import/prefer-default-export': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
+    'react/button-has-type': 0,
+    'react/no-unstable-nested-components': [2, { allowAsProps: true }],
+    'react/no-array-index-key': 0,
+    'class-methods-use-this': 0,
+    'no-param-reassign': 0,
+
+    'import/order': 0,
+    'simple-import-sort/exports': 1,
+    'simple-import-sort/imports': [
+      1,
+      {
+        groups: [
+          // External packages.
+          ['^'],
+          // Internal packages.
+          ['^@'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$']
+        ]
+      }
+    ]
+  }
 }
