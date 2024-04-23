6522ee963fd7673de4192085b27817e908780405
Valera <valerakaktotak@gmail.com>
2024-04-23 15:33:15 +0300

    added stucture folders and icons with images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
+import { Icons } from './assets/icons'
 
 export const App = () => {
   const [count, setCount] = useState(0)
 
-  return <></>
+  return (
+    <>
+      <img src={Icons.Close} alt='' />
+    </>
+  )
 }
---
+++ src/assets/icons.ts
@@ -0,0 +1,57 @@
+import ArrowDownGrey from './icons/arrow_down_grey.svg'
+import ArrowLeft from './icons/arrow_left.svg'
+import ArrowRight from './icons/arrow_right.svg'
+import ArrowUpGrey from './icons/arrow_up_grey.svg'
+import Bell from './icons/bell.svg'
+import CalendarGrey from './icons/calendar_grey.svg'
+import Close from './icons/close.svg'
+import ConsoleRed from './icons/console_red.svg'
+import Facebook from './icons/facebook.svg'
+import GridGrey from './icons/grid_grey.svg'
+import LanguageGrey from './icons/language_grey.svg'
+import LaptopRed from './icons/laptop_red.svg'
+import linkedin from './icons/linkedin.svg'
+import Menu from './icons/menu.svg'
+import Minus from './icons/minus.svg'
+import Play from './icons/play.svg'
+import Plus from './icons/plus.svg'
+import Search from './icons/search.svg'
+import SmartTVRed from './icons/smart_tv_red.svg'
+import SmartphoneRed from './icons/smartphone_red.svg'
+import Star from './icons/star.svg'
+import StarGrey from './icons/star_grey.svg'
+import StarRed from './icons/star_red.svg'
+import Streamvibe from './icons/streamvibe.png'
+import TabletRed from './icons/tablet_red.svg'
+import Twitter from './icons/twitter.svg'
+import VRSetsRed from './icons/vr_sets_red.svg'
+
+export const Icons = {
+  ArrowLeft,
+  ArrowRight,
+  Bell,
+  ConsoleRed,
+  LaptopRed,
+  Minus,
+  Play,
+  Plus,
+  Search,
+  SmartTVRed,
+  SmartphoneRed,
+  TabletRed,
+  VRSetsRed,
+  Facebook,
+  Twitter,
+  linkedin,
+  Streamvibe,
+  StarRed,
+  Star,
+  ArrowDownGrey,
+  ArrowUpGrey,
+  GridGrey,
+  StarGrey,
+  LanguageGrey,
+  CalendarGrey,
+  Menu,
+  Close
+}
