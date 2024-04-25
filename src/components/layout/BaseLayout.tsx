fd8a1ee4e1bf6f2c8549ca462de0f64a68e42c79
Valera <valerakaktotak@gmail.com>
2024-04-25 18:42:42 +0300

    created BaseLayout component structure

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,5 +1,19 @@
 import type { FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import { PageWrapper } from '../../styles/global/default'
+import { Footer } from '../footer/Footer'
+import { Header } from '../header/Header'
 
 export const BaseLayout: FC = () => {
-  return <div>BaseLayout</div>
+  return (
+    <PageWrapper>
+      <Header />
+      {/* main content */}
+      <section style={{ minHeight: '100vh' }}>
+        <Outlet />
+      </section>
+      {/* end of main content */}
+      <Footer />
+    </PageWrapper>
+  )
 }
