c04631f82cbd979ab2e3e493ea1bc1e950d1bddb
Valera <valerakaktotak@gmail.com>
2024-05-07 18:28:58 +0300

    created fetchShows request service

---
+++ src/redux/services/showsService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import urlConstants from '../../constant/urlConstants'
+
+const fetchAllShows = async (): Promise<ShowsData_Interface> => {
+  const response = await axios.get<ShowsData_Interface>(
+    urlConstants.fetchShowsUrl
+  )
+  return response.data
+}
+
+export const showsService = {
+  fetchAllShows
+}
