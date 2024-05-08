39bb4f282475bb04bcc1c4d12eebfc870599e3d5
Valera <valerakaktotak@gmail.com>
2024-05-08 14:08:30 +0300

    added vite.config file with change outDir name

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+export default {
+  build: {
+    outDir: 'build', // имя общей выходной директории
+    assetsDir: 'js' // имя директории со статикой
+    //assetsDir: './' // размещение статики внутри "dist"
+  }
+}
