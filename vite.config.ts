f3aca33e09aeb689c743995cbf7492405566817e
Valera <valerakaktotak@gmail.com>
2024-05-08 14:26:09 +0300

    deleted vercel and vite config files

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  build: {
-    outDir: 'build', // имя общей выходной директории
-    assetsDir: 'js' // имя директории со статикой
-    //assetsDir: './' // размещение статики внутри "dist"
-  }
-}
