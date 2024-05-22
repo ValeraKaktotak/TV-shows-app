d96b10675bdfc3c305f5552cb7a8bf388b8f5c67
Valera <valerakaktotak@gmail.com>
2024-05-22 23:16:42 +0300

    small change root.render in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,23 @@ import { BrowserRouter } from 'react-router-dom'
 import { App } from './App.tsx'
 import { persistor, store } from './redux/store'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <BrowserRouter>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <App />
+//       </PersistGate>
+//     </Provider>
+//   </BrowserRouter>
+// )
+
+// Найдите элемент с id 'root'
+const container = document.getElementById('root') as HTMLElement
+
+// Создайте корневой рендерер
+const root = ReactDOM.createRoot(container)
+
+root.render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
