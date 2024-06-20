2eb6ac2336c15793ce5026e0fb573509c99c3cb9
Valera <valerakaktotak@gmail.com>
2024-06-20 08:21:57 +0300

    another try for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,63 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import { ThemeProvider } from 'styled-components'
 import { persistor, store } from './redux/store'
-import { router } from './routes/routesConfig'
+
+import { App } from './App'
+import { BaseLayout } from './components'
+import routeConstants from './constant/routeConstants'
+import {
+  HomeScreen,
+  PageNotFound,
+  SearchScreen,
+  ShowDetailScreen,
+  ShowsScreen
+} from './screens'
 import { theme } from './styles/theme/theme'
 
-// const container = document.getElementById('root')
+//////////////////////////////
 
-// if (container) {
-//   const root = ReactDOM.createRoot(container)
-//   root.render(
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <RouterProvider router={router} />
-//       </PersistGate>
-//     </Provider>
-//   )
-// }
+const router = createBrowserRouter([
+  {
+    path: routeConstants.HOME,
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <BaseLayout />,
+        children: [
+          {
+            path: '/',
+            element: <HomeScreen />
+          },
+          {
+            path: routeConstants.SHOWS,
+            element: <ShowsScreen />
+          },
+          {
+            path: `${routeConstants.SHOWS}/:id`,
+            element: <ShowDetailScreen />
+          },
+          {
+            path: `${routeConstants.SHOWS}/genre/:genre`,
+            element: <ShowsScreen />
+          },
+          {
+            path: routeConstants.SEARCH,
+            element: <SearchScreen />
+          },
+          {
+            path: '*',
+            element: <PageNotFound />
+          }
+        ]
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
